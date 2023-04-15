bbe9c8159fc0052edba2a532569cb9a4c5751b88
netosk81 <hk.neto@gmail.com>
2023-04-15 10:10:27 -0300

    Colocando cardView

--- app/src/main/java/com/example/cardview/adapter/PostagemAdapter.java
+++ app/src/main/java/com/example/cardview/adapter/PostagemAdapter.java
@@ -1,5 +1,8 @@
 package com.example.cardview.adapter;
 
+import static android.os.Build.VERSION_CODES.R;
+
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,14 +11,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cardview.R;
+
+import java.sql.Ref;
+
 public class PostagemAdapter  extends RecyclerView.Adapter<PostagemAdapter.MyViewHolder> {
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
+        View itemLista = LayoutInflater.from(parent.getContext())
+                .inflate(com.example.cardview.R.layout.postagem_detalhe, parent,false);
 
-        return null;
+        return new MyViewHolder(itemLista);
     }
 
     @Override
@@ -30,9 +39,9 @@ public class PostagemAdapter  extends RecyclerView.Adapter<PostagemAdapter.MyVie
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
 
-        private TextView nome;
-        private TextView postagem;
-        private ImageView imagem;
+        private TextView    textNome;
+        private TextView    textPostagem;
+        private ImageView   imagemPostagem;
 
 
         public MyViewHolder(@NonNull View itemView) {
